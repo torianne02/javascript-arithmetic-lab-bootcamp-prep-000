e2a7070f262a5aa7ccde37feda7492f871d12390
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-13 15:51:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
